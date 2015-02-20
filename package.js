be3b7673ab4fb0ac7634d0dd1544a8066371aa92
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:15:28 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,14 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-button',
-  summary : 'Semantic UI - Button (official): Single component release of button',
-  version : '1.9.2',
+  summary : 'Semantic UI - Button: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Button.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'button.css'
+  ], 'client');
 });
