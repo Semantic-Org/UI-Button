7437f1ab9f41a85b9e607bba48fceba9f3857984
Pusher Robot <jack@semantic-ui.com>
2015-05-04 12:58:24 -0400

    Updated component to version 1.12.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-button',
   summary : 'Semantic UI - Button: Single component release',
-  version : '1.12.1',
+  version : '1.12.2',
   git     : 'git://github.com/Semantic-Org/UI-Button.git',
 });
 
